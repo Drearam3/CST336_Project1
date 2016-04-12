4836107f0c4e16fc00a264eded45b01241442b5c
drearam3 <andramirez@csumb.edu>
2016-04-12 03:33:09 +0000

    File for navigation bar

---
+++ navigation.php
@@ -0,0 +1,32 @@
+<!-- navbar -->
+<link rel = "stylesheet" href="main.css"> 
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+ 
+ 
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li>
+                    <div class="navbar-header">
+            <a class="navbar-brand" href="login.php">Movies</a>
+           
+        </div>
+                </li>
+                <li <?php echo $page_title=="movies" ? "class='active'" : ""; ?> >
+                    <a href="movies.php">Movies</a>
+                </li>
+                <li <?php echo $page_title=="Cart" ? "class='active'" : ""; ?> >
+                    <a href="cart.php">
+                        <?php
+                        // count products in cart
+                        $cart_count=count($_SESSION['cart_items']);
+                        ?>
+                        Cart <span class="badge" id="comparison-count"><?php echo $cart_count; ?></span>
+                    </a>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+ 
+    </div>
+</div>
+<!-- /navbar -->
